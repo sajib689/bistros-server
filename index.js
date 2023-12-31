ca45be819bc205e233c92769665dfadb9454562e
sajib689 <jibonraja11@gmail.com>
2023-12-31 22:12:44 +0600

    carts collections add

--- index.js
+++ index.js
@@ -45,6 +45,11 @@ async function run() {
       const result = await cartCollection.insertOne(item)
       res.send(result)
     })
+    // get cart collection
+    app.get('/carts', async (req, res) => {
+      const result = await cartCollection.find().toArray()
+      res.send(result)
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
