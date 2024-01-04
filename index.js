96c7a112e6c7b55d88511b6172e2c8df5fe32aef
sajib689 <jibonraja11@gmail.com>
2024-01-04 22:39:32 +0600

    delete

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config()
 // middleware
@@ -57,6 +57,13 @@ async function run() {
     // get cart collection
     app.get('/carts', async (req, res) => {
       const result = await cartCollection.find().toArray()
+      res.send(result)
+    });
+    // delete from carts
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await cartCollection.deleteOne(query)
       res.send(result)
     });
     // Send a ping to confirm a successful connection
